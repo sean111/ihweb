488b5a689702d385f24a77f7b4b9ef3a4b1dc74c
Sean Burke <sburke@8thwonder.io>
2017-09-06 22:36:46 -0400

    Some small additions and tweaks

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -70,4 +70,12 @@ class RouteServiceProvider extends ServiceProvider
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
+
+    protected function mapAdminRoutes()
+    {
+        Route::prefix('admin')
+            ->middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/admin.php'));
+    }
 }
