782054cadc9c28eccc667d5670c36e02fdd93344
Sean Burke <sburke@8thwonder.io>
2017-12-23 17:27:40 -0500

    Fix up edit schedule

--- resources/views/admin/schedule/edit.blade.php
+++ resources/views/admin/schedule/edit.blade.php
@@ -23,31 +23,31 @@
                         @endforeach
                     </select>
                 </div>
-                <div class="form-group schedule-date">
+                <div class="form-group schedule-date" style="{{ $event->frequency !== 'once' ? 'display: none;' : ''}}">
                     <label for="date" class="form-control-label">Date</label>
                     <input type="date" name="date" id="date" class="form-control">
                 </div>
-                <div class="form-group schedule-start" style="display: none;">
+                <div class="form-group schedule-start" style="{{ $event->frequency === 'once' ? 'display: none;' : ''}}">
                     <label for="start" class="form-control-label">Start Date</label>
-                    <input type="date" name="start" id="start" class="form-control" value="{{ $event->start ?? date('Y-m-d') }}">
+                    <input type="date" name="start" id="start" class="form-control" value="{{ $event->start ? date('Y-m-d', strtotime($event->start)) : date('Y-m-d') }}">
                 </div>
-                <div class="form-group schedule-end" style="display: none;">
+                <div class="form-group schedule-end" style="{{ $event->frequency === 'once' ? 'display: none;' : ''}}">
                     <label for="end" class="form-control-label">End Date</label>
-                    <input type="date" name="end" id="end" class="form-control" value="{{ $event->end ?? "" }}">
+                    <input type="date" name="end" id="end" class="form-control" value="{{ $event->end ? date('Y-m-d', strtotime($event->end)) : '' }}">
                 </div>
                 <div class="form-group schedule-days" style="display: none;">
                     <label for="days" class="form-control-label">Days:</label>
-                    <input type="checkbox" name="day[0]" value="1"> Monday
-                    <input type="checkbox" name="day[1]" value="1"> Tuesday
-                    <input type="checkbox" name="day[2]" value="1"> Wednesday
-                    <input type="checkbox" name="day[3]" value="1"> Thursday
-                    <input type="checkbox" name="day[4]" value="1"> Friday
-                    <input type="checkbox" name="day[5]" value="1"> Saturday
-                    <input type="checkbox" name="day[6]" value="1"> Sunday
+                    <input type="checkbox" name="day[0]" value="1" {{ !empty($event->days[0]) ? 'checked' : '' }}> Monday
+                    <input type="checkbox" name="day[1]" value="1" {{ !empty($event->days[1]) ? 'checked' : '' }}> Tuesday
+                    <input type="checkbox" name="day[2]" value="1" {{ !empty($event->days[2]) ? 'checked' : '' }}> Wednesday
+                    <input type="checkbox" name="day[3]" value="1" {{ !empty($event->days[3]) ? 'checked' : '' }}> Thursday
+                    <input type="checkbox" name="day[4]" value="1" {{ !empty($event->days[4]) ? 'checked' : '' }}> Friday
+                    <input type="checkbox" name="day[5]" value="1" {{ !empty($event->days[5]) ? 'checked' : '' }}> Saturday
+                    <input type="checkbox" name="day[6]" value="1" {{ !empty($event->days[6]) ? 'checked' : '' }}> Sunday
                 </div>
                 <div class="form-group">
                     <label for="time" class="form-control-label">Time</label>
-                    <input type="time" name="time" id="time" class="form-control">
+                    <input type="time" name="time" id="time" class="form-control" value="{{ $event->time ?? '' }}">
                 </div>
                 <div class="form-group pull-right">
                     <a href="{{ route('admin.schedules') }}" class="btn btn-danger">Cancel</a>
